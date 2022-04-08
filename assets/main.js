5ec4890380dcf34880fccae6565c59b1cda565df
cb-nandita <nandita@chargebee.com>
2022-04-08 12:15:46 +0530

    testing

--- assets/main.js
+++ assets/main.js
@@ -5,13 +5,13 @@ $(function() {
  metadata.settings.region ? metadata.settings.region.toLowerCase() : 'us';
  switch (region) {
   case 'au':
-    location.href = "https://app.au.chargebee.com/zendesk/ticket" + location.search;
+    location.href = "https://app.localcb.in:8443/zendesk/ticket" + location.search;
     break;
   case 'eu':
-    location.href = "https://app.eu.chargebee.com/zendesk/ticket" + location.search;
+    location.href = "https://app.localcb.in:8443/zendesk/ticket" + location.search;
     break;
   default :
-    location.href = "https://app.chargebee.com/zendesk/ticket" + location.search;
+    location.href = "https://app.localcb.in:8443/zendesk/ticket" + location.search;
     break;
  }
 });
